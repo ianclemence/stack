0faa4c3a6b563ab3a203856372d165b3452badc2
Ian Clemence <batricupid@gmail.com>
2025-10-16 00:53:10 +0700

    refactor: remove unused header styles from index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -17,18 +17,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  header: {
-    alignItems: 'center',
-    marginBottom: 40,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 8,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
-  },
 });
